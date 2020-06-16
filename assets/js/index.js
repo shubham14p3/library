a7e5701c31cf0776df9d462f7b1e1030e33ed2df
kip <cyruskiprop254@gmail.com>
2020-06-17 01:27:08 +0300

    setup linter files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
 const btnUtils = {
   deleteButton: (book) => {
-    let index = myLibrary.indexOf(book);
-    let btn = document.createElement("button");
-    btn.setAttribute("class", "btn btn-sm btn-warning");
-    btn.setAttribute("id", "delete-button");
-    btn.innerHTML = "Delete";
-    btn.addEventListener("click", function () {
+    const index = myLibrary.indexOf(book);
+    const btn = document.createElement('button');
+    btn.setAttribute('class', 'btn btn-sm btn-warning');
+    btn.setAttribute('id', 'delete-button');
+    btn.innerHTML = 'Delete';
+    btn.addEventListener('click', () => {
       btnUtils.handleDeleteBook(btn, index);
     });
     return btn;
   },
 
   handleDeleteBook: (btn, index) => {
-    let row = btn.parentNode.parentNode;
+    const row = btn.parentNode.parentNode;
     myLibrary.splice(index, 1);
     row.parentNode.removeChild(row);
     tableUtils.resetTable();
@@ -20,11 +20,11 @@ const btnUtils = {
   },
 
   toggleReadBtn: (book) => {
-    let btn = document.createElement("button");
-    btn.setAttribute("class", "btn btn-sm btn-info");
+    const btn = document.createElement('button');
+    btn.setAttribute('class', 'btn btn-sm btn-info');
     btn.innerHTML = book.read;
 
-    btn.addEventListener("click", function () {
+    btn.addEventListener('click', () => {
       if (book.read == true) {
         currentIdx = myLibrary.indexOf(book);
         myLibrary[currentIdx].read = false;
@@ -48,25 +48,25 @@ const btnUtils = {
 
 const tableUtils = {
   resetTable: () => {
-    table = document.getElementById("table");
-    tr = table.getElementsByTagName("tr");
+    table = document.getElementById('table');
+    tr = table.getElementsByTagName('tr');
     for (let i = tr.length - 1; i >= 1; i--) {
       tr[i].remove();
     }
   },
 
   createTable: () => {
-    let theaders = ["Title", "Author", "Pages", "Read"];
+    const theaders = ['Title', 'Author', 'Pages', 'Read'];
 
-    const table = document.createElement("table");
-    table.setAttribute("id", "table");
-    table.setAttribute("class", "table-sm table table-dark");
+    const table = document.createElement('table');
+    table.setAttribute('id', 'table');
+    table.setAttribute('class', 'table-sm table table-dark');
 
-    let tr = table.insertRow(-1);
+    const tr = table.insertRow(-1);
 
     theaders.map((item) => {
-      let th = document.createElement("th");
-      th.setAttribute("class", "col");
+      const th = document.createElement('th');
+      th.setAttribute('class', 'col');
       th.innerHTML = item;
       tr.appendChild(th);
     });
@@ -75,28 +75,28 @@ const tableUtils = {
   },
 
   addRows: (database) => {
-    let table = document.getElementById("table");
+    const table = document.getElementById('table');
 
-    let rowCount = table.rows.length;
+    const rowCount = table.rows.length;
 
     database.map((book, index) => {
-      let tr = table.insertRow(rowCount);
-      tr.setAttribute("data-row-index", index);
+      const tr = table.insertRow(rowCount);
+      tr.setAttribute('data-row-index', index);
 
       Object.values(book).map((element, index) => {
-        let td = document.createElement("td");
+        let td = document.createElement('td');
         td = tr.insertCell(index);
         td.innerHTML = element;
-        td.setAttribute("id", index);
+        td.setAttribute('id', index);
       });
 
-      let actionTd = document.createElement("td");
+      let actionTd = document.createElement('td');
       actionTd = tr.insertCell(book.length);
 
-      let deleteButton = btnUtils.deleteButton(book);
-      let toggleButton = btnUtils.toggleReadBtn(book);
+      const deleteButton = btnUtils.deleteButton(book);
+      const toggleButton = btnUtils.toggleReadBtn(book);
 
-      deleteButton.setAttribute("data-book-index", index);
+      deleteButton.setAttribute('data-book-index', index);
       actionTd.appendChild(toggleButton);
       actionTd = tr.insertCell(book.length);
       actionTd.appendChild(deleteButton);
@@ -106,17 +106,17 @@ const tableUtils = {
 
 const formUtils = {
   showForm: () => {
-    let form = document.getElementById("add-book-form");
-    form.setAttribute("class", "form-container");
+    const form = document.getElementById('add-book-form');
+    form.setAttribute('class', 'form-container');
   },
 
   handleSubmit: (event) => {
     event.preventDefault();
-    let form = document.getElementById("add-book-form");
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+    const form = document.getElementById('add-book-form');
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
 
     data = {
       title,
@@ -127,51 +127,53 @@ const formUtils = {
 
     const { validData, errors } = validateUtils.validateInputs(data);
 
-    console.log(Object.values(errors));
+    const { authorError, pagesError, titleError } = errors;
     if (
-      errors.authorError.length == 0 &&
-      errors.titleError.length == 0 &&
-      errors.pagesError.length == 0
+      authorError.length == 0
+      && titleError.length == 0
+      && pagesError.length == 0
     ) {
       addBookToLibrary(validData);
 
       tableUtils.resetTable();
       tableUtils.addRows(myLibrary);
-      alert("successfully added the item");
+      alert('successfully added the item');
     } else {
       Object.keys(errors).map((key) => {
         if (errors[key] !== []) {
-          let span = document.getElementById(key);
+          const span = document.getElementById(key);
           console.log(key);
-          span.setAttribute("class", "error-message");
+          span.setAttribute('class', 'error-message');
           errors[key].map((error) => (span.innerHTML = error));
         }
       });
-      alert("Error while adding the book");
+      alert('Error while adding the book');
     }
     form.reset();
 
-    form.setAttribute("class", "d-none !important");
+    form.setAttribute('class', 'd-none !important');
   },
 
   submitForm: () => {
-    let form = document.getElementById("add-book-form");
-    form.addEventListener("submit", formUtils.handleSubmit);
+    const form = document.getElementById('add-book-form');
+    form.addEventListener('submit', formUtils.handleSubmit);
   },
 };
 
 const validateUtils = {
   isEmpty: (data, errors) => {
-    data.title ? true : errors.titleError.push("Title can't be blank");
-    data.pages ? true : errors.pagesError.push("Pages can't be blank");
-    data.author ? true : errors.authorError.push("Author can't be blank");
+    const { title, pages, author } = data;
+    const { titleError, pagesError, authorError } = errors;
+    title ? true : titleError.push("Title can't be blank");
+    pages ? true : pagesError.push("Pages can't be blank");
+    author ? true : authorError.push("Author can't be blank");
 
     return errors;
   },
 
   validateNum: (num, errors) => {
-    if (Number(num) === NaN && num !== "") {
-      errors.pagesError.push("Please Insert a number");
+    if (Number(num) === NaN && num !== '') {
+      errors.pagesError.push('Please Insert a number');
     }
     return errors;
   },
@@ -195,21 +197,21 @@ const validateUtils = {
 
 let myLibrary = [
   {
-    title: "48 laws of power",
-    author: "Awesome Raj",
+    title: '48 laws of power',
+    author: 'Awesome Raj',
     pages: 45,
     read: false,
   },
   {
-    title: "Introduction to programming for newbies",
-    author: "Cyrus Kiprop",
+    title: 'Introduction to programming for newbies',
+    author: 'Cyrus Kiprop',
     pages: 65,
     read: true,
   },
 
   {
-    title: "Microverse Data Structure",
-    author: "Ariel Camus",
+    title: 'Microverse Data Structure',
+    author: 'Ariel Camus',
     pages: 67,
     read: true,
   },
@@ -232,12 +234,12 @@ function addBookToLibrary(book) {
 }
 
 function newBook() {
-  let btn = document.getElementById("new-book");
-  btn.addEventListener("click", formUtils.showForm);
+  const btn = document.getElementById('new-book');
+  btn.addEventListener('click', formUtils.showForm);
 }
 
 const render = () => {
-  let div = document.getElementById("container");
+  const div = document.getElementById('container');
   table = tableUtils.createTable();
 
   div.appendChild(table);
